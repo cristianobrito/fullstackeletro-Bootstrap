ae3b5794f861bffe4f6d11e5809ea574d65e6f28
cristiano <britonano@gmail.com>
2020-11-05 23:25:49 -0300

    Add files via upload

---
+++ FSE-V1/script/funcoes.js
@@ -0,0 +1,32 @@
+    function exibir_categoria(categorias)          
+    {
+        let elementos = document.getElementsByClassName('col');
+        console.log(elementos);                     
+        for(var i=0; i<elementos.length; i++)
+        {
+            console.log(elementos[i].id); 
+            if(categorias == elementos[i].id)       
+            elementos[i].style =  "display:block";  
+            else
+            elementos[i].style ="display:none";    
+        }
+    }
+
+    let exibir_todos = () =>                      
+    {
+        let elementos = document.getElementsByClassName('col');
+
+        for(var i=0; i<elementos.length; i++)
+        {
+            elementos[i].style =  "display:block";
+        }
+    };
+
+    let destaque = (imagem) => {                
+            console.log(imagem);                 
+            if(imagem.width == 200)              
+                imagem.width = 150;              
+            else                                
+                imagem.width = 200;
+    };
+    
---
+++ FSE-V1/script/script.js
@@ -0,0 +1,4 @@
+    function exibir_categoria()
+    {
+
+    }
